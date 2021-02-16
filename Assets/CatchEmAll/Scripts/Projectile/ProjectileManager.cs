867542b80e8401ea54cc509d3250d4c064bbc5f7
senevsemih <senevsemih@gmail.com>
2021-02-16 21:50:21 +0300

    created : projectile manager

---
+++ Assets/CatchEmAll/Scripts/Projectile/ProjectileManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.Projectile
+{
+    public class ProjectileManager : MonoBehaviour
+    {
+        [SerializeField] private ProjectileSettings _projectileSettings;
+
+        private Rigidbody2D _projectileRb;
+
+        private void Awake()
+        {
+            _projectileRb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            ProjectileMovement();
+        }
+
+        private void ProjectileMovement()
+        {
+            float angle = Mathf.Atan2(_projectileRb.velocity.y, _projectileRb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}

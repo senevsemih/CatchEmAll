7dffc48fb6ff4a4f36f231cc648bede203edebd7
senevsemih <senevsemih@gmail.com>
2021-02-16 21:54:09 +0300

    created : gun manager

---
+++ Assets/CatchEmAll/Scripts/Gun/GunManager.cs
@@ -0,0 +1,42 @@
+﻿using Assets.CatchEmAll.Scripts.Input;
+using Assets.CatchEmAll.Scripts.Projectile;
+using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.Gun
+{
+    public class GunManager : MonoBehaviour
+    {
+        [SerializeField] private ProjectileSettings _projectileSettings;
+        [SerializeField] private InputData _ınputData;
+
+        [Header("PROJECTILE")] 
+        [SerializeField] private GameObject _projectilePrefab;
+        [SerializeField] private Transform _projectileTransform;
+
+        private Transform _gunPivot;
+
+        private void Awake()
+        {
+            _gunPivot = GetComponentInParent<Transform>().parent;
+        }
+        
+        private void Update()
+        {
+            GunRotation();
+        }
+
+        private void GunRotation()
+        {
+            Vector2 distance = _ınputData.MousePosition - transform.position;
+            float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+            _gunPivot.rotation = Quaternion.Euler(0f, 0f, angle); 
+        }
+
+        public void FireProjectile()
+        {
+            GameObject projectile = Instantiate(_projectilePrefab);
+            projectile.transform.position = _projectileTransform.position;
+            projectile.GetComponent<Rigidbody2D>().velocity = transform.right * _projectileSettings.ProjectileSpeed;
+        }
+    }
+}

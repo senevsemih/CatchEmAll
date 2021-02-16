4cb8519b5e715e78222060cdc326a40fa9b6f29d
senevsemih <senevsemih@gmail.com>
2021-02-16 21:50:04 +0300

    created : projectile settings

---
+++ Assets/CatchEmAll/Scripts/Projectile/ProjectileSettings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.Projectile
+{
+    [CreateAssetMenu(fileName = "New Projectile Settings",
+        menuName = "CatchEmAll/Projectile Settings")]
+    public class ProjectileSettings : ScriptableObject
+    {
+        [SerializeField] private ProjectileStates ProjectileStates
+            = ProjectileStates.Default;
+        [SerializeField] private float _projectileSpeed;
+        [SerializeField] private float _projectileAngle;
+        
+        
+        public ProjectileStates States
+        {
+            get => ProjectileStates;
+            set => ProjectileStates = value;
+        }
+
+        public float ProjectileSpeed => _projectileSpeed;
+        public float ProjectileAngle => _projectileAngle;
+    }
+}
+
+public enum ProjectileStates
+{
+    Default,
+    Bouncy,
+    FireBall
+}

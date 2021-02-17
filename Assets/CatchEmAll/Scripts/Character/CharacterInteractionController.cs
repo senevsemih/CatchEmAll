9954cbd07e285f42dd84191dff199ca37ecf3476
senevsemih <senevsemih@gmail.com>
2021-02-17 23:25:44 +0300

    created : ınteraction controller script

---
+++ Assets/CatchEmAll/Scripts/Character/CharacterInteractionController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Assets.CatchEmAll.Scripts.Projectile;
+using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.Character
+{
+    public class CharacterInteractionController : MonoBehaviour
+    {
+        [SerializeField] private ProjectileSettings _projectileSettings;
+        [SerializeField] private GameObject _gun;
+        
+        private const float LerpSpeed = 3f;
+
+        [Header("Follow Area")] 
+        [SerializeField] private List<Transform> _targets = new List<Transform>();
+        [SerializeField] private List<Transform> _collectables = new List<Transform>();
+
+        private void Update()
+        {
+            if (_collectables.Count <= 0) return;
+            for (int i = 0; i < _collectables.Count; i++)
+            {
+                _collectables[i].position =
+                    Vector3.Lerp(_collectables[i].position, _targets[i].position, LerpSpeed * Time.deltaTime); 
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (GetTag(other, "Collectable/Gun"))
+            {
+                _gun.SetActive(true);
+                Destroy(other.gameObject, .1f);
+            }
+            
+            if (GetTag(other, "Projectile/Default"))
+            {
+                _projectileSettings.States = ProjectileStates.Default;
+                Collect(other);
+            }
+            
+            if (GetTag(other, "Projectile/Bouncy"))
+            {
+                _projectileSettings.States = ProjectileStates.Bouncy;
+                Collect(other);
+            }
+            
+            if (GetTag(other, "Projectile/Fireball"))
+            {
+                _projectileSettings.States = ProjectileStates.FireBall;
+                Collect(other);
+            }
+        }
+
+        private void Collect(Collider2D other)
+        {
+            other.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            
+            if (!_collectables.Contains(other.gameObject.transform))
+            {
+                _collectables.Add(other.gameObject.transform);
+            }
+        }
+
+        private bool GetTag(Collider2D other, string otherName)
+        {
+            return other.gameObject.tag == otherName ? true : false;
+        }
+    }
+}

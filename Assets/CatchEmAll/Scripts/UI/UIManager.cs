d9bb1d8ba5b6a74350f2ee80afdd87cefd4d1754
senevsemih <senevsemih@gmail.com>
2021-02-21 00:01:25 +0300

    uı manager

---
+++ Assets/CatchEmAll/Scripts/UI/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.CatchEmAll.Scripts.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private UISettings _uıSettings;
+        [SerializeField] private GameObject[] _keyIcons;
+        [SerializeField] private GameObject _endScene;
+
+        private void Update()
+        {
+            if (_uıSettings.Keys.Count == 0) return;
+            for (int i = 0; i < _uıSettings.Keys.Count; i++)
+            {
+                _keyIcons[i].SetActive(true);
+            }
+
+            if (_uıSettings.End)
+            {
+                TheEnd();
+            }
+        }
+
+        private void TheEnd()
+        {
+            _endScene.SetActive(true);
+            Invoke("LoadScene", 15f);
+        }
+
+        private void LoadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
+

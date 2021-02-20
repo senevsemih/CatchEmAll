d4c3903f0f39c004d32fe63d6d9cb6fed548e918
senevsemih <senevsemih@gmail.com>
2021-02-21 00:01:19 +0300

    uı settings

---
+++ Assets/CatchEmAll/Scripts/UI/UISettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.UI
+{
+    [CreateAssetMenu(fileName = "New UI Settings",
+        menuName = "CatchEmAll/UI Settings")]
+    public class UISettings : ScriptableObject
+    {
+        [SerializeField] private List<GameObject> _keys;
+        [SerializeField] private bool _end;
+
+        public List<GameObject> Keys
+        {
+            get => _keys;
+            set => _keys = value;
+        }
+
+        public bool End
+        {
+            get => _end;
+            set => _end = value;
+        }
+    }
+}

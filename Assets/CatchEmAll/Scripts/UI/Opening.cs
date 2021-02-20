ea3043bd78bc565d16fc2772bad0025bad90b5e7
senevsemih <senevsemih@gmail.com>
2021-02-21 00:01:50 +0300

    uı opening

---
+++ Assets/CatchEmAll/Scripts/UI/Opening.cs
@@ -0,0 +1,17 @@
+﻿using Assets.CatchEmAll.Scripts.Character;
+using UnityEngine;
+
+namespace Assets.CatchEmAll.Scripts.UI
+{
+    public class Opening : MonoBehaviour
+    {
+        [SerializeField] private GameObject _character;
+        [SerializeField] private GameObject _openingUI;
+        
+        public void StartGame()
+        {
+            _character.GetComponent<CharacterManager>().enabled = true;
+            _openingUI.SetActive(false);
+        }
+    }
+}
